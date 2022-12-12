57477272822dc83700f9380a344648b4a7ac2e39
Corey Skidmore <skidmoreeco@gmail.com>
2022-12-12 10:14:13 -0500

    initial commit + setting Apollo Server

---
+++ Develop/server/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+app.use(routes);
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
+};
+
+startApolloServer(typeDefs, resolvers);
+
+
