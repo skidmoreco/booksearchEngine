a9898aa56f7b7eece477e6c9ea142e2b3eb3caea
Corey Skidmore <skidmoreeco@gmail.com>
2022-12-12 12:34:45 -0500

    setting resolvers & mutations

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,42 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Book } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolver = {
+    Query: {
+        users: async () => {
+            return User.find().populate('books');
+        },
+        user: async (parent, {username}) => {
+            return User.findOne({ username }).populate('books');
+        }
+    },
+
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('No user found with this email address');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        saveBook: async (parent, { title, author}, context) => {
+            if (context.book) {
+                
+            }
+        }
+    }
+}
