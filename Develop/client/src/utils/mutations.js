95424d23226757212460e5b3562e762763c73c57
Corey Skidmore <skidmoreeco@gmail.com>
2022-12-13 14:47:00 -0500

    wrapped up mutations & setting apollo

---
+++ Develop/client/src/utils/mutations.js
@@ -0,0 +1,45 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email. password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($userId: ID!, bookId: String!) {
+        saveBook(userId: $userId, bookId: $bookId) {
+            bookId
+            user {
+                _id
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($book: String!,) {
+        removeBook(book: $book) {
+            bookId
+            
+        }
+    }
+`
