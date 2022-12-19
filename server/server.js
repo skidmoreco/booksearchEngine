fc0ed2923f9ce92c4b6910f911edc6f3278bfde6
skidmoreco <106204668+skidmoreco@users.noreply.github.com>
2022-12-18 20:38:04 -0500

    debugging const deployment issues

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-  console.log(`GraphQL server ready at http://localhost:${PORT}${server.graphqlPath}`);
-});
+// db.once('open', () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+//   console.log(`GraphQL server ready at http://localhost:${PORT}${server.graphqlPath}`);
+// });
